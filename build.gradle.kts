46d6923f599c353120f97c4cfa4595f1090c9ea6
Kilemonn <kylegonzalez2255@gmail.com>
2024-06-12 12:20:21 +0900

    Bump to Kotlin 2.0.0 compiler. Update dependencies. Remove maven publish configuration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,41 +1,12 @@
 plugins {
-    kotlin("jvm") version "1.9.0"
+    kotlin("jvm") version "2.0.0"
     `java-library`
-    `maven-publish`
     jacoco
 }
 
 group = "com.github.Kilemonn"
 version = "0.1.6" // Make sure this version matches the release version in the repo
 java.sourceCompatibility = JavaVersion.VERSION_17
-
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = group.toString()
-            artifactId = rootProject.name
-            version = version
-
-            from(components["java"])
-
-            pom {
-                name.set("Mock All")
-                description.set("An extension library built on-top of mockito used to quickly mock all injected beans.")
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("kilemonn")
-                    }
-                }
-            }
-        }
-    }
-}
 
 repositories {
     mavenCentral()
@@ -47,10 +18,10 @@ dependencies {
     implementation("javax.annotation:javax.annotation-api:1.3.2")
 
     // https://mvnrepository.com/artifact/jakarta.annotation/jakarta.annotation-api
-    implementation("jakarta.annotation:jakarta.annotation-api:2.1.1")
+    implementation("jakarta.annotation:jakarta.annotation-api:3.0.0")
 
     // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-test
-    implementation("org.springframework.boot:spring-boot-starter-test:3.2.3")
+    implementation("org.springframework.boot:spring-boot-starter-test:3.3.0")
 
     testImplementation("org.mockito:mockito-inline:5.2.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
